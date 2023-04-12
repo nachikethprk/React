fb06fdd33788959d69fd8b0661454a01492159e4
Nachiketh. R <126313865+nachikethprk@users.noreply.github.com>
2023-04-12 22:14:29 +0530

    Add files via upload

---
+++ Components/Flipkart.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import STYLE from "./Flipkart.module.css"
+const Flipkart=()=>{
+    return(
+        <>
+        <div id={STYLE.aa}>
+            <img src="https://1000logos.net/wp-content/uploads/2021/02/Flipkart-logo.png" alt="" />
+            <ul id={STYLE.bb}>
+        <li><a href="">Login</a></li>
+        <li><a href=""> Become a seller</a></li>
+        <li><a href="">More</a></li>
+        <li><a href="">Kart</a></li>
+       </ul>
+        </div>
+        </>
+
+    )
+}
+export default Flipkart
